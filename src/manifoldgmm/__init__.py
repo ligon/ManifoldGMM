b6ce12c3425ddc0091d0d2f054b2e70abb41cc0a
Ethan Ligon <ligon@berkeley.edu>
2025-10-24 17:22:12 -0700

    Initial dummy __init__.py

---
+++ src/manifoldgmm/__init__.py
@@ -0,0 +1,8 @@
+"""
+ManifoldGMM: Generalized Method of Moments estimation on Riemannian manifolds.
+
+This package currently provides placeholders for future geometry and
+econometrics modules. See the documentation in ``docs/`` for design notes.
+"""
+
+__all__ = []
